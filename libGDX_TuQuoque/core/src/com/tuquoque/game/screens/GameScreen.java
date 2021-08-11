dc75d3d279309982e2661f3972fc46d8d58b5b89
AlleMors <64963770+AlleMors@users.noreply.github.com>
2021-08-11 11:20:44 +0200

    Modifica GameScreen e aggiunta assets

--- libGDX_TuQuoque/core/src/com/tuquoque/game/screens/GameScreen.java
+++ libGDX_TuQuoque/core/src/com/tuquoque/game/screens/GameScreen.java
@@ -2,72 +2,81 @@ package com.tuquoque.game.screens;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
-import com.badlogic.gdx.physics.box2d.*;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.tuquoque.game.GameStarter;
 
-import static com.tuquoque.game.GameStarter.*;
+import java.awt.*;
 
 public class GameScreen extends AbstractScreen {
-    private final BodyDef bodyDef;
-    private final FixtureDef fixtureDef;
+
+    final  private SpriteBatch batch;
+
+    final private Animation<TextureRegion> walkRightAnimation;
+    final private Animation<TextureRegion> walkLeftAnimation;
+    final private Animation<TextureRegion> idleRightAnimation;
+    private final Animation<TextureRegion> idleLeftAnimation;
+
+    float elapsedTime;
+    private final Rectangle player;
+
+    // True = right, False = left
+    private boolean direction=true;
 
     public GameScreen(final GameStarter context){
         super(context);
 
-        bodyDef = new BodyDef();
-        fixtureDef = new FixtureDef();
+        batch = new SpriteBatch();
 
-        //CIRCLE
-        bodyDef.position.set(8, 9);
-        bodyDef.gravityScale = 1;
-        bodyDef.type = BodyDef.BodyType.DynamicBody;
-        Body body = world.createBody(bodyDef);
+        player=new Rectangle();
 
-        fixtureDef.isSensor = false;
-        fixtureDef.restitution = 0.5f;
-        fixtureDef.friction = 0.2f;
-        fixtureDef.filter.categoryBits = BIT_CIRCLE;
-        fixtureDef.filter.maskBits = BIT_GROUND | BIT_BOX;
-        CircleShape cShape = new CircleShape();
-        cShape.setRadius(0.5f);
-        fixtureDef.shape = cShape;
-        body.createFixture(fixtureDef);
-        cShape.dispose();
+        player.height=32;
+        player.width=32;
 
-        //BOX
-        bodyDef.position.set(8.9f, 4);
-        bodyDef.gravityScale = 1;
-        bodyDef.type = BodyDef.BodyType.DynamicBody;
-        body = world.createBody(bodyDef);
+        player.x=1280/2-32/2;
+        player.y=720/2-32/2;
 
-        fixtureDef.isSensor = false;
-        fixtureDef.restitution = 0;
-        fixtureDef.friction = 0.3f;
-        fixtureDef.filter.categoryBits = BIT_BOX;
-        fixtureDef.filter.maskBits = BIT_CIRCLE | BIT_GROUND;
-        PolygonShape pShape = new PolygonShape();
-        pShape.setAsBox(0.5f, 0.5f);
-        fixtureDef.shape = pShape;
-        body.createFixture(fixtureDef);
-        pShape.dispose();
+        Texture imgRight = new Texture("Gladiator-Sprite Sheet.png");
+        Texture imgLeft = new Texture("Gladiator-Sprite Sheet-Left.png");
 
-        //PLATFORM
-        bodyDef.position.set(8f, 2);
-        bodyDef.gravityScale = 0;
-        bodyDef.type = BodyDef.BodyType.StaticBody;
-        body = world.createBody(bodyDef);
+        OrthographicCamera camera = new OrthographicCamera();
+        camera.setToOrtho(false,1280,720);
 
-        fixtureDef.isSensor = false;
-        fixtureDef.restitution = 0;
-        fixtureDef.friction = 0.2f;
-        fixtureDef.filter.categoryBits = BIT_GROUND;
-        fixtureDef.filter.maskBits = -1;
-        pShape = new PolygonShape();
-        pShape.setAsBox(7.9f, 0.5f);
-        fixtureDef.shape = pShape;
-        body.createFixture(fixtureDef);
-        pShape.dispose();
+
+        TextureRegion[][] tmpFramesRight =TextureRegion.split(imgRight, 32,32);
+        TextureRegion[][] tmpFramesLeft =TextureRegion.split(imgLeft, 32,32);
+
+
+        TextureRegion[] walkFramesRight = new TextureRegion[8];
+        TextureRegion[] walkFramesLeft = new TextureRegion[8];
+        TextureRegion[] idleFramesRight = new TextureRegion[5];
+        TextureRegion[] idleFramesLeft = new TextureRegion[5];
+
+        int index=0;
+
+        for(int i=0;i<8;i++){
+            walkFramesRight[index]= tmpFramesRight[1][i];
+            walkFramesLeft[index]= tmpFramesLeft[1][i];
+            index++;
+        }
+
+        index=0;
+
+        for(int i=0;i<5;i++){
+            idleFramesLeft[index]= tmpFramesLeft[0][i];
+            idleFramesRight[index]= tmpFramesRight[0][i];
+            index++;
+        }
+
+        walkRightAnimation =new Animation<>(1/8f, walkFramesRight);
+        walkLeftAnimation=new Animation<>(1/8f, walkFramesLeft);
+        idleLeftAnimation=new Animation<>(1/5f, idleFramesLeft);
+        idleRightAnimation=new Animation<>(1/5f, idleFramesRight);
     }
 
     @Override
@@ -77,14 +86,59 @@ public class GameScreen extends AbstractScreen {
 
     @Override
     public void render(float delta) {
-        ScreenUtils.clear(0, 0, 0, 1);
-        if(Gdx.input.isKeyPressed(Input.Keys.ESCAPE)){
-            context.setScreen(ScreenType.LOADING);
+        elapsedTime+=Gdx.graphics.getDeltaTime();
+        Gdx.gl.glClearColor(0,0,0,1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT|GL20.GL_DEPTH_BUFFER_BIT);
+        batch.begin();
+
+
+        if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)){
+            player.x+=150*Gdx.graphics.getDeltaTime();
+            batch.draw(walkRightAnimation.getKeyFrame(elapsedTime,true),player.x,player.y);
+            direction=true;
+        }
+        else if (Gdx.input.isKeyPressed(Input.Keys.LEFT)){
+            player.x-=100*Gdx.graphics.getDeltaTime();
+            batch.draw(walkLeftAnimation.getKeyFrame(elapsedTime,true),player.x,player.y);
+            direction=false;
         }
 
-        viewport.apply();
-        world.step(delta, 6, 2);
-        box2DDebugRenderer.render(world, viewport.getCamera().combined);
+        else if(Gdx.input.isKeyPressed(Input.Keys.DOWN)){
+            player.y-=100*Gdx.graphics.getDeltaTime();
+            if(direction)
+                batch.draw(walkRightAnimation.getKeyFrame(elapsedTime,true),player.x,player.y);
+            else
+                batch.draw(walkLeftAnimation.getKeyFrame(elapsedTime,true),player.x,player.y);
+        }
+
+        else if(Gdx.input.isKeyPressed(Input.Keys.UP)){
+            player.y+=150*Gdx.graphics.getDeltaTime();
+            if(direction)
+                batch.draw(walkRightAnimation.getKeyFrame(elapsedTime,true),player.x,player.y);
+            else
+                batch.draw(walkLeftAnimation.getKeyFrame(elapsedTime,true),player.x,player.y);
+        }
+
+        else{
+            if(direction)
+                batch.draw(idleRightAnimation.getKeyFrame(elapsedTime,true),player.x, player.y);
+            else
+                batch.draw(idleLeftAnimation.getKeyFrame(elapsedTime,true),player.x, player.y);
+        }
+
+        if(player.x<0)
+            player.x=0;
+
+        if(player.y<0)
+            player.y=0;
+
+        if(player.x+32>1280)
+            player.x=1280-32;
+
+        if(player.y+32>720)
+            player.y=720-32;
+
+        batch.end();
     }
 
     @Override
