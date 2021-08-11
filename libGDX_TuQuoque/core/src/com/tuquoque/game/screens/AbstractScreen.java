6b17167656544e1d8b25170abf4b0906a1daedc7
martinverr <martinverrascina@gmail.com>
2021-08-11 16:40:20 +0200

    Use batch from context instead of initializing a new one Then converted pixel to world unit(batch is combined with camera) TODO: speed fix

--- libGDX_TuQuoque/core/src/com/tuquoque/game/screens/AbstractScreen.java
+++ libGDX_TuQuoque/core/src/com/tuquoque/game/screens/AbstractScreen.java
@@ -1,6 +1,7 @@
 package com.tuquoque.game.screens;
 
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
 import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.utils.viewport.FitViewport;
@@ -11,10 +12,12 @@ public abstract class AbstractScreen implements Screen {
    protected final FitViewport viewport;
    protected final World world;
    protected final Box2DDebugRenderer box2DDebugRenderer;
+   protected final SpriteBatch batch;
 
    public AbstractScreen(final GameStarter context){
        this.context = context;
        viewport = context.getViewport();
+       batch = context.getBatch();
        this.world = context.getWorld();
        this.box2DDebugRenderer = context.getBox2DDebugRenderer();
    }
--- libGDX_TuQuoque/core/src/com/tuquoque/game/screens/GameScreen.java
+++ libGDX_TuQuoque/core/src/com/tuquoque/game/screens/GameScreen.java
@@ -2,22 +2,23 @@ package com.tuquoque.game.screens;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
-import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.tuquoque.game.GameStarter;
 
 import java.awt.*;
 
+
+/*
+* batch initialized in AbstractScreen.java instead of in GameScreen.java
+* conversion from pixel to world unit
+* TODO: fix speed, Rectangle.x Rectangle.x are integers, and speed*deltatime is rounded to 0 if speed is below 100
+* */
+
 public class GameScreen extends AbstractScreen {
-
-    final  private SpriteBatch batch;
-
     final private Animation<TextureRegion> walkRightAnimation;
     final private Animation<TextureRegion> walkLeftAnimation;
     final private Animation<TextureRegion> idleRightAnimation;
@@ -32,15 +33,13 @@ public class GameScreen extends AbstractScreen {
     public GameScreen(final GameStarter context){
         super(context);
 
-        batch = new SpriteBatch();
-
         player=new Rectangle();
 
-        player.height=32;
-        player.width=32;
+        player.height=1;
+        player.width=1;
 
-        player.x=1280/2-32/2;
-        player.y=720/2-32/2;
+        player.x=0;
+        player.y=0;
 
         Texture imgRight = new Texture("player/Gladiator-Sprite Sheet.png");
         Texture imgLeft = new Texture("player/Gladiator-Sprite Sheet-Left.png");
@@ -80,6 +79,23 @@ public class GameScreen extends AbstractScreen {
         ScreenUtils.clear(0, 0, 0, 1);
     }
 
+    private void handleInputPlayerMov(){
+        if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)){
+            player.x+=150*Gdx.graphics.getDeltaTime();
+            direction=true;
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)){
+            player.x-=150*Gdx.graphics.getDeltaTime();
+            direction=false;
+        }
+        if(Gdx.input.isKeyPressed(Input.Keys.DOWN)){
+            player.y-=150*Gdx.graphics.getDeltaTime();
+        }
+        if(Gdx.input.isKeyPressed(Input.Keys.UP)){
+            player.y+=150*Gdx.graphics.getDeltaTime();
+        }
+    }
+
     @Override
     public void render(float delta) {
         elapsedTime+=Gdx.graphics.getDeltaTime();
@@ -87,52 +103,25 @@ public class GameScreen extends AbstractScreen {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT|GL20.GL_DEPTH_BUFFER_BIT);
         batch.begin();
 
+        //handling input for player movement
+        handleInputPlayerMov();
 
-        if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)){
-            player.x+=150*Gdx.graphics.getDeltaTime();
-            batch.draw(walkRightAnimation.getKeyFrame(elapsedTime,true),player.x,player.y);
-            direction=true;
-        }
-        else if (Gdx.input.isKeyPressed(Input.Keys.LEFT)){
-            player.x-=100*Gdx.graphics.getDeltaTime();
-            batch.draw(walkLeftAnimation.getKeyFrame(elapsedTime,true),player.x,player.y);
-            direction=false;
-        }
+        //drawing player
+        if(direction)
+            batch.draw(idleRightAnimation.getKeyFrame(elapsedTime,true),player.x, player.y,1,1);
+        else
+            batch.draw(idleLeftAnimation.getKeyFrame(elapsedTime,true),player.x, player.y,1,1);
 
-        else if(Gdx.input.isKeyPressed(Input.Keys.DOWN)){
-            player.y-=100*Gdx.graphics.getDeltaTime();
-            if(direction)
-                batch.draw(walkRightAnimation.getKeyFrame(elapsedTime,true),player.x,player.y);
-            else
-                batch.draw(walkLeftAnimation.getKeyFrame(elapsedTime,true),player.x,player.y);
-        }
 
-        else if(Gdx.input.isKeyPressed(Input.Keys.UP)){
-            player.y+=150*Gdx.graphics.getDeltaTime();
-            if(direction)
-                batch.draw(walkRightAnimation.getKeyFrame(elapsedTime,true),player.x,player.y);
-            else
-                batch.draw(walkLeftAnimation.getKeyFrame(elapsedTime,true),player.x,player.y);
-        }
-
-        else{
-            if(direction)
-                batch.draw(idleRightAnimation.getKeyFrame(elapsedTime,true),player.x, player.y);
-            else
-                batch.draw(idleLeftAnimation.getKeyFrame(elapsedTime,true),player.x, player.y);
-        }
-
+        // collision player-screenBorder -> player reposition
         if(player.x<0)
             player.x=0;
-
         if(player.y<0)
             player.y=0;
-
-        if(player.x+32>1280)
-            player.x=1280-32;
-
-        if(player.y+32>720)
-            player.y=720-32;
+        if(player.x+1>16)
+            player.x=16-1;
+        if(player.y+1>9)
+            player.y=9-1;
 
         batch.end();
     }
