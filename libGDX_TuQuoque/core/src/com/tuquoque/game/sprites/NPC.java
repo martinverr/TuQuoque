69f9e18ecdb4bf6b4137fca393d806b447113e86
AlleMors <64963770+AlleMors@users.noreply.github.com>
2021-08-21 09:59:17 +0200

    updated map borders

--- libGDX_TuQuoque/core/src/com/tuquoque/game/sprites/NPC.java
+++ libGDX_TuQuoque/core/src/com/tuquoque/game/sprites/NPC.java
@@ -1,13 +1,9 @@
 package com.tuquoque.game.sprites;
 
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.Circle;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.BodyDef;
-import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.World;
-import com.tuquoque.game.GameStarter;
 
 public class NPC extends Entity {
 
