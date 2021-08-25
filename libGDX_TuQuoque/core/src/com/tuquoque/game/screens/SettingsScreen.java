e474c28d230713ed9559a0db1ba3dc024cad06d6
AlleMors <64963770+AlleMors@users.noreply.github.com>
2021-08-25 20:09:41 +0200

    bozza SettingsScreen

--- libGDX_TuQuoque/core/src/com/tuquoque/game/screens/SettingsScreen.java
+++ libGDX_TuQuoque/core/src/com/tuquoque/game/screens/SettingsScreen.java
@@ -1,8 +1,21 @@
 package com.tuquoque.game.screens;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.Camera;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.utils.Scaling;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.tuquoque.game.GameStarter;
 import com.tuquoque.game.input.GameKeys;
 import com.tuquoque.game.input.InputListener;
@@ -14,8 +27,34 @@ import com.tuquoque.game.input.InputManager;
 */
 
 public class SettingsScreen extends AbstractScreen implements InputListener {
+
+    private Stage stage;
+    private Image music_ON_active;
+    private Image music_ON_inactive;
+    private Scaling scaling;
+
     public SettingsScreen(GameStarter context) {
         super(context);
+
+        // Setting up stage
+        stage=new Stage(new FitViewport(1280,720));
+        Gdx.input.setInputProcessor(stage);
+
+        // Actors
+        music_ON_active=new Image(new Texture(Gdx.files.internal("buttons/music_ON_red.png")));
+        music_ON_inactive=new Image(new Texture(Gdx.files.internal("buttons/music_ON_white.png")));
+        stage.addActor(music_ON_active);
+        stage.addActor(music_ON_inactive);
+
+        // Setting up actors
+        music_ON_active.setHeight(music_ON_active.getHeight()/2);
+        music_ON_active.setWidth(music_ON_active.getWidth()/2);
+        music_ON_active.setPosition(200,500);
+
+        music_ON_inactive.setHeight(music_ON_inactive.getHeight()/2);
+        music_ON_inactive.setWidth(music_ON_inactive.getWidth()/2);
+        music_ON_inactive.setPosition(200,500);
+
     }
 
     @Override
@@ -25,14 +64,19 @@ public class SettingsScreen extends AbstractScreen implements InputListener {
 
     @Override
     public void show() {
-        ScreenUtils.clear(1, 1, 1, 1);
+        ScreenUtils.clear(0, 0, 0, 1);
         inputManager.addInputListener(this);
     }
 
     @Override
     public void render(float delta) {
-        ScreenUtils.clear(1, 1, 1, 1);
+        ScreenUtils.clear(0, 0, 0, 1);
+        stage.act(delta);
+        stage.draw();
+    }
 
+    public void resize(int width, int height){
+        stage.getViewport().update(width,height);
     }
 
     @Override
@@ -47,12 +91,12 @@ public class SettingsScreen extends AbstractScreen implements InputListener {
 
     @Override
     public void hide() {
-        inputManager.removeInputListener(this);
+
     }
 
     @Override
     public void dispose() {
-
+        stage.dispose();
     }
 
     @Override
