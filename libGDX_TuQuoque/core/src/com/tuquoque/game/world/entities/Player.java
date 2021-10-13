18642e2aefa1df5c375c708bcf13a31f9d4b2860
martinverr <martinverrascina@gmail.com>
2021-10-13 11:23:38 +0200

    Player stats added (health, mana, exp, level, maxHealth...) -getters and setters of these stats -methods levelUp(), updateGameUI(), fillHealth(), fillMana(), addExp() -Constructor now requires GameUI

--- libGDX_TuQuoque/core/src/com/tuquoque/game/world/entities/Player.java
+++ libGDX_TuQuoque/core/src/com/tuquoque/game/world/entities/Player.java
@@ -4,8 +4,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.*;
-import com.badlogic.gdx.utils.Array;
-import com.tuquoque.game.ui.Item;
+import com.tuquoque.game.ui.GameUI;
 import com.tuquoque.game.world.entities.animation.PlayerAnimation;
 
 
@@ -14,9 +13,16 @@ public class Player extends Entity {
     private final World world;
     private final float B2D_BODY_DEF_WIDTH = 0.4f;
     private final float B2D_BODY_DEF_HEIGHT = 0.6f;
+    private final GameUI gameUI;
 
-    private Array<Item> playerInv;
+    private float maxHealth;
+    private float maxMana;
+    private float maxExp;
 
+    private float health;
+    private float mana;
+    private float exp;
+    private float level;
 
     /**
     * Constructor of Player
@@ -26,13 +32,22 @@ public class Player extends Entity {
      * @param world Box2D World where the player will be defined
      * @param coords coordinates of where the player will be spawned
     * */
-    public Player(World world, Vector2 coords){
+    public Player(World world, Vector2 coords, GameUI gameUI){
         //offset to legs = - heightEntireBox + heightLegs / 2
         super(world, coords, 0.4f, 0.15f, -0.6f +0.15f/2);
         this.world = world;
-        playerBodyDef();
+        this.gameUI = gameUI;
 
-        playerInv = new Array<>(15);
+        health = 100;
+        maxHealth = 100;
+        mana = 50;
+        maxMana = 100;
+        exp = 0;
+        maxExp = 100;
+        level = 1;
+        updateGameUI();
+
+        playerBodyDef();
         setNpcAnimation(new PlayerAnimation(this));
     }
 
@@ -47,11 +62,82 @@ public class Player extends Entity {
         sensorShape.dispose();
     }
 
-
-    public Array<Item> getPlayerInv() {
-        return playerInv;
+    private void updateGameUI(){
+        gameUI.setBars(health/maxHealth, exp/maxExp, mana/maxMana);
     }
 
+    public void fillHealth(){
+        health = maxHealth;
+    }
+
+    public void fillMana(){
+        mana = maxMana;
+    }
+
+    public void levelUp(){
+        level += 1;
+        maxHealth += 50;
+        maxMana += 10;
+        maxExp *= 1.5f;
+        fillHealth();
+        fillMana();
+        updateGameUI();
+    }
+
+    public void setHealth(float health) {
+        if(health >= 0 && health <= maxHealth)
+            this.health = health;
+        else
+            throw new IllegalArgumentException("Tried to set health to \"" + health + "\", but not in range: 0-" + maxHealth);
+    }
+
+    public void setMana(float mana) {
+        if(mana >= 0 && mana <= maxMana)
+            this.mana = mana;
+        else
+            throw new IllegalArgumentException("Tried to set mana to \"" + mana + "\", but not in range: 0-" + maxMana);
+    }
+
+    public void addExp(float exp) {
+        if(exp < 0)
+            throw new IllegalArgumentException("exp must be a positive value");
+        this.exp += exp;
+
+        //recursive call addExp if exp exceed
+        if(exp > maxExp){
+            float expInExceed = exp-maxExp;
+            levelUp();
+            addExp(expInExceed);
+        }
+    }
+
+    public float getLevel() {
+        return level;
+    }
+
+    public float getHealth() {
+        return health;
+    }
+
+    public float getMana() {
+        return mana;
+    }
+
+    public float getExp() {
+        return exp;
+    }
+
+    public float getMaxHealth() {
+        return maxHealth;
+    }
+
+    public float getMaxMana() {
+        return maxMana;
+    }
+
+    public float getMaxExp() {
+        return maxExp;
+    }
 
     public void draw(Batch batch, float elapsedTime) {
         if (batch.isDrawing()) {
